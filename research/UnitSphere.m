d2a0f741ef424db4af2ebd573e8ad814c0f17ebe
whizkidrohan <whizkidrohan@gmail.com>
2016-10-02 17:43:20 -0400

    Still debugging with quadratic formula in paper

--- research/UnitSphere.m
+++ research/UnitSphere.m
@@ -1,6 +1,6 @@
 function UnitSphere
 x1=0;
-v1=1;
+v1=0;
 a_max=1;
 
 % returns the minimum time between 2 points ([x1,y1] and [x2,y2]) in the state space
@@ -10,23 +10,26 @@ function T = cal_min_time(x2, v2)
     sigma = sign(x2 - x1 - dp_acc);
     a2 = -sigma*a_max;
     a1 = -a2;
-    qa = a1;
-    qb = 2*v1;
-    qc = (v2^2-v1^2)/(2*a2) - (x2-x1);
-    ta1_a = (-qb + (qb^2-4*qa*qc)^0.5)/(2*qa);
-    ta1_b = (-qb - (qb^2-4*qa*qc)^0.5)/(2*qa);
-    if imag(ta1_a) ~= 0 && imag(ta1_b) ~= 0 
-        ta1 = -1;
-    elseif ta1_a > 0
+    a = a1;
+    b = 2*v1;
+    c = (v2^2-v1^2)/(2*a2) - (x2-x1);
+    q = -0.5*(b + sign(b)*(b^2-4*a*c)^0.5);
+    ta1_a = q/a; ta1_b = c/q;
+    if(ta1_a >= 0);
         ta1 = ta1_a;
-    elseif ta1_a == 0 && ta1_b < 0
-        ta1 = ta1_a;
-    elseif ta1_b >= 0
+    elseif (ta1_b >= 0)
         ta1 = ta1_b;
-    else
-        ta1=-1; %random large negative value
+    elseif (ta1_a==0 && ta1_b==0)
+        T=0;
+        return;
+    else 
+        T=-1;
+        return;
     end
     T = (v2-v1)/a2 + 2*ta1;
+    if T<0
+        disp('stuff');
+    end
 end
 
 x = x1-3.0:0.02:x1+3.0;
