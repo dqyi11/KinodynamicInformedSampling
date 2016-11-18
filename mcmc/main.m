260444f0facd34e2c15e2c54b3916cbcd2fbdbeb
cole.gulino <cole.gulino@gmail.com>
2016-11-18 03:25:16 -0500

    MCM info

--- mcmc/main.m
+++ mcmc/main.m
@@ -1,6 +1,6 @@
 %% Main variables
 global x0 h x1 v1 x2 v2 a_max;
-x1 = 0; v1=0; x2=1; v2=0; a_max=1;
+x1 = 0; v1=0; x2=1; v2=1; a_max=1;
 num_dim = 2;
 x0 = zeros(1,num_dim);
 h = 0.001;
@@ -11,17 +11,19 @@ alpha = 0.01;
 surf_step = 0.01;
 no_epochs = 1000;
 T_best = 5;
-no_trials = 1;
+no_trials = 20;
 last_of_run = [];
 %% Running gradient descent to the level set 
 % Generate a random starting point
 
+results = cell(no_trials,1);
+per_in_total = 0;
 for trial = 1:no_trials
     % Choose a random starting point
     start = (high_range - low_range).*rand(1,num_dim) + low_range;
     
     % Gradient descent to the level-curve
-    results = grad_descent(no_epochs, start, alpha, T_best, @cal_min_time1);
+    results{trial} = grad_descent(no_epochs, start, alpha, T_best, @cal_min_time1);
     
     % Random walk
     % Parameters (may need to tune or randomize these)
@@ -29,8 +31,12 @@ for trial = 1:no_trials
     max_steps = 2000;
     rand_step_prob = 0.2;
 %     results = [results; random_walk([results(end,1), results(end,2)], max_steps, alpha)];
-    results_after = results(end,1:end-1);
-    results = [results; mcmc(results_after, max_steps, sigma, @cal_min_time1, T_best)];
+    results_after = results{trial}(end,1:end-1);
+    [mcmc_results, per_in] = mcmc(results_after, max_steps, sigma, @cal_min_time1, T_best);
+    
+    per_in_total = per_in_total + per_in;
+    
+    results{trial} = [results{trial}; mcmc_results];
     last_of_run = [last_of_run; results(end,:)];
 
     trial
@@ -38,10 +44,20 @@ end
 %% Plot results
 figure;
 % Z = plot_surf(low_range:surf_step:high_range, low_range:surf_step:high_range, @cal_min_time3);
-plot_kino_surf(low_range:surf_step:high_range, low_range:surf_step:high_range, @cal_min_time3, T);
-plot3(results(:,1),results(:,2),results(:,3),'r');
+colors = [hsv(100); jet(100)];
+shuffled_rows = randi(size(colors,1), [no_trials, 1]);
+colors = colors(shuffled_rows, :);
+
+plot_kino_surf(low_range:surf_step:high_range, low_range:surf_step:high_range, @cal_min_time3, T_best);
+
+for trial = 1:no_trials
+    plot3(results{trial}(:,1),results{trial}(:,2),results{trial}(:,3), 'Color', colors(trial,:));
+end
 % scatter3(results(:,1),results(:,2),results(:,3),'r');
 hold off;
+
+fprintf('Average Percentage In \n');
+per_in_total / no_trials
 
 % figure;
 % hold on;
@@ -54,9 +70,9 @@ hold off;
 % percent_in = no_in / no_trials
 
 % Find average distance
-a = -1;
-b = 1;
-r = a + (b-a)*rand(no_trials, size(last_of_run, 2) - 1);
-avg_dist_uniform = avg_distance_bw_points(r);
-avg_dist_sampling = avg_distance_bw_points(last_of_run(:,1:2));
-avg_dist_ratio = avg_dist_sampling / avg_dist_uniform
+% a = -1;
+% b = 1;
+% r = a + (b-a)*rand(no_trials, size(last_of_run, 2) - 1);
+% avg_dist_uniform = avg_distance_bw_points(r);
+% avg_dist_sampling = avg_distance_bw_points(last_of_run(:,1:2));
+% avg_dist_ratio = avg_dist_sampling / avg_dist_uniform
--- mcmc/mcmc.m
+++ mcmc/mcmc.m
@@ -1,25 +1,12 @@
-function [ results ] = mcmc( x, max_steps, sigma, cstfxn, T_best )
+function [ results, per_in] = mcmc( x, max_steps, sigma, cstfxn, T_best )
     global h
     % Randomly choose the number of steps
-%     no_steps = round((max_steps).*rand(1,1) - 1);
     no_steps = max_steps;
     z = 1;
     results = [];
     no_accepted = 0;
     for step = 1:no_steps
-        % Randomly choose a direction or go with gradient
-%         if( rand() > rand_step_prob )
-%             rand_dir = 2*rand(1,size(x,2)) - 1;
-%             posx = [];
-%             posx = x - sigma*rand_dir;
-%             posz = my_dist(posx);
-%         else
-%             grad = my_grad(@my_dist, x, h);
-%             posx = [];
-%             posx = x - sigma*grad;
-%             posz = my_dist(posx);
-%         end
-%         
+        % Randomly choose a direction or go with gradient 
         posx = [];
         posx = x + normrnd(0, sigma, size(x));
         posz = feval(cstfxn, posx);
@@ -29,10 +16,7 @@ function [ results ] = mcmc( x, max_steps, sigma, cstfxn, T_best )
             z = posz;
             results = [results; [x,z]];
             no_accepted = no_accepted + 1;
-        else
-            fprintf('Rejected\n');
         end
     end
-    fprintf('Percent accepted\n');
-    no_accepted / no_steps
+    per_in = no_accepted / no_steps
 end
