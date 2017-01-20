4d7336006029ad5d5805dd43244b2544d7695617
Cole Gulino <cole.gulino@gmail.com>
2017-01-19 21:43:15 -0500

    Added get_infeasible_time_interval

--- cpp_src/include/Dimt/Dimt.h
+++ cpp_src/include/Dimt/Dimt.h
@@ -1,5 +1,12 @@
 #pragma once
+
+// Standard libarary
 #include <iostream>
+#include <utility> // std::pair
+#include <limits> // std::numeric_limits::infinity()
+#include <algorithm> // std::max and std::min
+
+// Eigen
 #include <Eigen/Dense>
 using Eigen::VectorXd;
 
@@ -46,8 +53,11 @@ public:
 	    VectorXd Ts(int(x1.size()/2));
 	    for(int i = 0; i<x1.size(); i=i+2)
 	    {
-	      Ts(i/2) = get_min_time_1dof(x1(i), x1(i+1), x2(i), x2(i+1), xi(i), xi(i+1));
+	    	Ts(i/2) = get_min_time_1dof(x1(i), x1(i+1), x2(i), x2(i+1), xi(i), xi(i+1));
 	    }
+
+	    // std::cout << "Time Before: " << Ts.maxCoeff() << std::endl;
+	    // std::cout << "Time Other way: " << get_min_time(x1, xi) + get_min_time(xi, x2) << std::endl;
 	    return Ts.maxCoeff();
 	}
 
@@ -65,12 +75,12 @@ public:
 		// 	std::string error_msg = "Invalid Arguments to get_min_time";
 		// 	throw std::invalid_argument(error_msg);
 		// }
-	    VectorXd Ts(int(x1.size()/2));
-	    for(int i = 0; i<x1.size(); i=i+2)
-	    {
-	      Ts(i/2) = get_min_time_1dof(x1(i), x1(i+1), x2(i), x2(i+1));
-	    }
-	    return Ts.maxCoeff();
+		VectorXd Ts(int(x1.size()/2));
+		for(int i = 0; i < x1.size(); i = i + 2)
+		{
+			Ts(i/2) = get_min_time_1dof(x1(i), x1(i+1), x2(i), x2(i+1));
+		}
+		return Ts.maxCoeff();
 	}
 
 	// returns the minimum time between 3 points ([x1,y1] -> [xi,yi] -> [x2,y2]) in the state space
@@ -78,10 +88,10 @@ public:
 	double get_min_time_1dof(const double x1, const double v1, const double x2,
 			const double v2, const double xi, const double vi) const
 	{
-    double T1 = get_min_time_1dof(x1, v1, xi, vi);
-    double T2 = get_min_time_1dof(xi, vi, x2, v2);
-    if(T1 < 0 || T2 < 0) return -1;
-    return T1 + T2;
+		double T1 = get_min_time_1dof(x1, v1, xi, vi);
+		double T2 = get_min_time_1dof(xi, vi, x2, v2);
+		if(T1 < 0 || T2 < 0) return -1;
+		return T1 + T2;
 	}
 
 	// returns the minimum time between 2 points ([x1,y1] and [x2,y2]) in the state space
@@ -90,18 +100,86 @@ public:
 			const double v2) const
 	{
 		double ta1;
-    double dp_acc = 0.5*(v1+v2)*std::abs(v2-v1)/a_max_;
-    double sigma = sign(x2 - x1 - dp_acc);
-    double a2 = -sigma*a_max_;  double a1 = -a2;
-    double a = a1; double b = 2*v1; double c = (v2*v2-v1*v1)/(2*a2) - (x2-x1);
-    double q = -0.5*(b + sign(b)*std::sqrt(b*b-4*a*c));
-    double ta1_a = q/a; double ta1_b = c/q;
-    if (ta1_a > 0) ta1 = ta1_a;
-    else if (ta1_b > 0) ta1 = ta1_b;
-    else if (ta1_a == 0 || ta1_b == 0) return 0;
-    else return -1;
-    return (v2-v1)/a2 + 2*ta1;
-  }
+		const double dp_acc = 0.5*(v1+v2)*std::abs(v2-v1)/a_max_;
+		const double sigma = sign(x2 - x1 - dp_acc);
+		const double a2 = -sigma*a_max_;  const double a1 = -a2;
+		const double a = a1; const double b = 2*v1; 
+		const double c = (v2*v2-v1*v1)/(2*a2) - (x2-x1);
+		const double q = -0.5*(b + sign(b)*std::sqrt(b*b-4*a*c));
+		const double ta1_a = q/a; const double ta1_b = c/q;
+		if (ta1_a > 0) ta1 = ta1_a;
+		else if (ta1_b > 0) ta1 = ta1_b;
+		else if (ta1_a == 0 || ta1_b == 0) return 0;
+		else return -1;
+		return (v2-v1)/a2 + 2*ta1;
+	}
+
+	///
+	/// Function to determine if the minimum time problem violates the infeasible
+	/// time regions of any of the joints
+	///
+	/// @param joint_vector Vector of all of the joints
+	/// @param slowest_dof DOF that has the slowest time
+	/// @param min_time Slowest time of the joints
+	/// @return Tuple (is_violation, new_min_time) that represents where the first element tells if there
+	/// is a violation or not and the second element gives the new minimum time
+	///
+  	std::pair<bool, double> check_infeasible_time(const VectorXd& joint_vector, 
+  												  const int& slowest_dof,
+  												  const double min_time) const
+  	{
+
+  	}
+
+	/// 
+	/// Function to get the infeasible time_interval for one DOF
+	///
+	/// @param x1 start position
+	/// @param v1 start velocity
+	/// @param x2 end position
+	/// @param v2 end velocity
+	/// return A pair of values (min_time, max_time) is infinity if not in region
+	///
+	std::pair<double, double> get_infeasible_time_interval(const double x1, 
+														   const double v1, 
+														   const double x2,
+														   const double v2) const
+	{
+		// Calculate a1 and a2 with reverse signs as descussed in the paper:
+		// http://www.tobiaskunz.net/pubs/KunzIROS14-DimtRrt.pdf
+		double ta1;
+		const double dp_acc = 0.5 * (v1 + v2) * std::abs(v2 - v1) / a_max_;
+		const double distance = x2 - x1 - dp_acc;
+		const double sigma = sign(distance);
+		const double a2 = sigma*a_max_;  
+		const double a1 = -a2;
+
+		if(v1 * v2 <= 0.0 or a1 * v1 < 0.0) 
+		{
+			// No infeasible time interval, because it is not in the correct region
+			return std::pair<double, double>(std::numeric_limits<double>::infinity(),
+											 std::numeric_limits<double>::infinity());
+		}
+
+		// Again determine if there is an infeasible time interval
+		const double zeroTime1 = std::abs(v1) / a_max_;
+		const double zeroTime2 = std::abs(v2) / a_max_;
+		const double zeroDistance = zeroTime1 * v1 / 2.0 + zeroTime2 * v2 / 2.0;
+		if(std::abs(zeroDistance) < std::abs(distance))
+		{
+			// No infeasible time interval, because it is not in the correct region
+			return std::pair<double, double>(std::numeric_limits<double>::infinity(),
+											 std::numeric_limits<double>::infinity());
+		}
+
+		// If it is in the right region, return the infesible time interval
+		const double a = a1; const double b = 2*v1; 
+		const double c = (v2 * v2 - v1 * v1) / (2 * a2) - (x2 - x1);
+		const double q = -0.5 * (b + sign(b) * std::sqrt(b * b - 4 * a * c));
+		const double ta1_a = q/a; const double ta1_b = c/q;
+		return std::pair<double, double>(std::min(ta1_a, ta1_b), 
+										 std::max(ta1_a, ta1_b));
+	}
 
 private:
 	template<typename T>
---
+++ cpp_src/src/RejectionSampler.cpp
@@ -0,0 +1,77 @@
+#include <Sampler/RejectionSampler.h>
+
+///
+/// RejectionSampler
+///
+
+///
+/// Get a series of samples for the problem space
+///
+/// @param no_samples Number of samples to get
+/// @param time Boolean that determines if the time to run the proccess is displayed 
+/// @return A series of samples of shape (number of samples, sample dimension)
+///
+MatrixXd RejectionSampler::sample(const int& no_samples, const bool& time) const
+{
+	// Get the limits of the space
+	VectorXd max_vals, min_vals;
+	std::tie(max_vals, min_vals) = problem().state_limits();
+	double max = max_vals(0); double min = min_vals(0);
+
+	// Run until you get the correct number of samples
+	int curr_no_samples = 0;
+	MatrixXd samples(no_samples, problem().start_state().size() + 1);
+
+	// If you want to time the sampling
+	high_resolution_clock::time_point t1;
+	if(time) t1 = high_resolution_clock::now();
+
+	while(curr_no_samples < no_samples)
+	{
+		VectorXd sample = get_random_sample(max, min, problem().start_state().size());
+
+		if(problem().is_in_level_set(sample))
+		{
+			VectorXd newsample(problem().start_state().size() + 1);
+			newsample << sample, problem().get_cost(sample);
+			samples.row(curr_no_samples) = newsample;
+			curr_no_samples++;
+			// std::cout << curr_no_samples << std::endl;
+		}
+	}
+
+    // If you want to time the sampling and display it
+    if(time)
+    {
+		high_resolution_clock::time_point t2 = high_resolution_clock::now();
+		auto duration_s = duration_cast<seconds>( t2 - t1 ).count();
+		auto duration_ms = duration_cast<milliseconds>( t2 - t1 ).count();
+		auto duration_us = duration_cast<microseconds>( t2 - t1 ).count();
+		if (duration_s != 0)
+			std::cout << "Total Sampling Time: " << duration_s << "s" << std::endl;
+		else if (duration_ms != 0)
+			std::cout << "Total Sampling Time: " << duration_ms << "ms" << std::endl;
+		else 
+			std::cout << "Total Sampling Time: " << duration_us << "us" << std::endl;
+    }
+
+    return samples;
+}
+
+// Can implement as many private functions as you want to help do the sampling
+VectorXd RejectionSampler::get_random_sample(const double& max, const double& min, const int& size) const
+{
+	// Set up the random number generator
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<> dis(min, max);
+
+	VectorXd sample(size);
+
+	for(int i = 0; i < size; i++)
+	{
+		sample(i) = dis(gen);
+	}
+
+	return sample;
+}
