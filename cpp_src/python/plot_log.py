7a392b69bca693b6614cf74d9793b43e3b2e6dfb
Cole Gulino <cole.gulino@gmail.com>
2017-02-19 18:18:15 -0500

    Changed the plotting function to take in two filenames as command line arguments instead of fixed filenames

--- cpp_src/python/plot_log.py
+++ cpp_src/python/plot_log.py
@@ -1,21 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 
-with open('../test_mcmc.log') as f:
-	mcmc = np.loadtxt(f)
-with open('../test_rej.log') as f:
-	rej = np.loadtxt(f)
+def main():
+    parser = argparse.ArgumentParser(description='Plot the results from the sampling methods.')
+    parser.add_argument('--f_rej', help='Filename of HRS or Rejection Sampling.')
+    parser.add_argument('--f_mc', help='Filename of MCMC or HMC samples.')
 
-print(mcmc.shape)
-print(rej.shape)
+    args = parser.parse_args()
+    with open(args.f_mc) as f:
+        mcmc = np.loadtxt(f)
+    with open(args.f_rej) as f:
+        rej = np.loadtxt(f)
 
-for joint in range(0, mcmc.shape[1] - 1, 2):
-	fig = plt.figure(joint / 2)
-	plt.scatter(mcmc[:,joint], mcmc[:,joint+1], c='b', marker="o", label='MCMC')
-	plt.scatter(rej[:,joint], rej[:,joint+1], c='r', marker="o", label='Rejection')
-	plt.legend(loc='upper left')
-	plt.title('Joint {}'.format(joint / 2))
-	plt.xlabel('x')
-	plt.ylabel('x_dot')
+    print(mcmc.shape)
+    print(rej.shape)
 
-plt.show()
+    for joint in range(0, mcmc.shape[1] - 1, 2):
+        joint_num = int(joint / 2)
+        fig = plt.figure(joint_num)
+        plt.scatter(mcmc[:,joint], mcmc[:,joint+1], c='b', marker="o", label='MCMC')
+        plt.scatter(rej[:,joint], rej[:,joint+1], c='r', marker="o", label='Rejection')
+        plt.legend(loc='upper left')
+        plt.title('Joint {}'.format(joint_num))
+        plt.xlabel('x')
+        plt.ylabel('x_dot')
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()
