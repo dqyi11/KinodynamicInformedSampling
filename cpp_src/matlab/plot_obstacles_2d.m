ebe14230dbe353481a1a3b730b7ad93ba76e5f79
Daqing Yi <daqingy@ava.personalrobotics.ri.cmu.edu>
2017-06-23 15:35:38 -0400

    add matlabplot

--- cpp_src/matlab/plot_obstacles_2d.m
+++ cpp_src/matlab/plot_obstacles_2d.m
@@ -1,19 +1,5 @@
 
-
-
-obs_cube_center = [0.0, 0.0; 2.0, 1.2; -2.0, 1.2; -2.0, -1.2; 2.0, -1.2];
-obs_cube_center1 = [-2. 3.5; -2, -3.5; 2, 3.5; 2, -3.5];
-obs_cube_center2 = [-4, 2.; -4, -2.; 4., 2.; 4., -2];
-obs_cube_center3 = [0.0, -3.; 0.0, 3.];
-obs_cube_radius = [1.0, 1.0; 0.5, 0.5; 0.5, 0.5; 0.5, 0.5; 0.5, 0.5];
-obs_cube_radius1 = [0.5, 0.5; 0.5, 0.5; 0.5, 0.5; 0.5, 0.5];
-obs_cube_radius2 = [0.5, 0.5; 0.5, 0.5; 0.5, 0.5; 0.5, 0.5];
-obs_cube_radius3 = [0.5, 1.0; 0.5, 1.0];
-
-obs_cube_center = [obs_cube_center; obs_cube_center1; obs_cube_center2; obs_cube_center3];
-obs_cube_radius = [obs_cube_radius; obs_cube_radius1; obs_cube_radius2; obs_cube_radius3];
-
-num = 5+4+4+2;
+obstacles_data = loadjson(['.' filesep 'obstacles.json']);
 
 start = [-4, -4];
 goal = [4, 4];
@@ -21,9 +7,12 @@ goal = [4, 4];
 figure(11);
 hold on;
 rectangle('Position', [-5, -5. 10, 10], 'FaceColor', [1., 1., 1.]);
-for i=1:num
-    rect = [obs_cube_center(i,1)-obs_cube_radius(i,1), obs_cube_center(i,2)-obs_cube_radius(i,2), 2*obs_cube_radius(i,1),2*obs_cube_radius(i,2)];
-    rectangle('Position', rect,'FaceColor',[.0 .0 .0]);
+for i=1:1:size(obstacles_data.obstacles,2)
+    obs = obstacles_data.obstacles{1,i};
+    if obs.type == 'hypercube'
+        rect = [obs.center(1,1)-obs.radius(1,1), obs.center(1,2)-obs.radius(1,2), 2*obs.radius(1,1),2*obs.radius(1,2)];
+        rectangle('Position', rect,'FaceColor',[.0 .0 .0]);
+    end
 end
 scatter(start(1,1), start(1,2), 'rs');
 scatter(goal(1,1), goal(1,2), 'bs');
